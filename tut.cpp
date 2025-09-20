32bc2977fd898119432189a7aa88c510b8680835
Raja-Aliyan <aliyanr5421@gmail.com>
2025-09-20 07:12:12 -0700

    My first Code

---
+++ tut.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+
+    float cgpa;
+
+    int rollNo;
+
+    cout << "type your name:";
+    cin >> name;
+
+    cout << "what is your cgpa:";
+    cin >> cgpa;
+
+    cout << "type your roll no:";
+    cin >> rollNo;
+
+    cout << "----YOUR INFORMATION----" << endl;
+    cout << "your name:" << name << endl;
+    cout << "your cgpa:" << cgpa << endl;
+    cout << "your roll no:" << rollNo << endl;
+}
